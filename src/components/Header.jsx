b00335f6e3f5abead64fa3d07bb1c52e902d5637
akino23tie <asjad23ti@mahasiswa.pcr.ac.id>
2025-07-04 21:49:53 +0700

    tambah team

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,7 @@ import { FiSearch, FiUser } from "react-icons/fi";
 import { FaGem } from "react-icons/fa";
 
 export default function Header() {
-  const navItemClass = ({ isActive }) =>
-    `block w-full text-left text-sm px-4 py-2 rounded-lg transition font-medium ${
-      isActive ? "bg-black text-white" : "text-gray-700 hover:bg-gray-200"
-    }`;
+  const navItemClass = ({ isActive }) => `block w-full text-left text-sm px-4 py-2 rounded-lg transition font-medium ${isActive ? "bg-black text-white" : "text-gray-700 hover:bg-gray-200"}`;
 
   return (
     <aside className="fixed top-0 left-0 h-screen w-64 bg-white shadow-md flex flex-col justify-between z-50 border-r border-gray-200">
